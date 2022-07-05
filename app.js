1c0a57e7c73e31a990cc848adc123cd18f4f9406
Adil Bulsari <adilbulsari@gmail.com>
2022-07-05 14:12:29 +0100

    changes done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { getTopic } = require("./controller/getTopic.controller");
-const { getArticleByiD } = require("./controller/getArticle.controller");
+const { getArticleById } = require("./controller/getArticle.controller");
 
 const app = express();
 
 app.get("/api/topics", getTopic);
-app.get("/api/articles/:article_id", getArticleByiD);
+app.get("/api/articles/:article_id", getArticleById);
 
 module.exports = app;
