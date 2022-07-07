7138f48a553dbb7915cbcdc1b69b0221ef832cf0
Adil Bulsari <adilbulsari@gmail.com>
2022-07-07 10:24:07 +0100

    done get api/articles

--- model/articles.model.js
+++ model/articles.model.js
@@ -5,7 +5,7 @@ exports.fetchAllArticles = () => {
     .query(
       ` SELECT articles.*,CAST(COUNT(comments.article_id) AS INTEGER) AS comment_count FROM comments
      JOIN articles ON articles.article_id=comments.article_id
-     GROUP BY articles.article_id;`
+     GROUP BY articles.article_id ORDER BY articles.created_at;`
     )
     .then((data) => {
       console.log(data.rows);
