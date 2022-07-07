49754ff88ae8c66813bc9e16958726782d54e7c3
Adil Bulsari <adilbulsari@gmail.com>
2022-07-07 15:16:50 +0100

    resolved tests and jest-sorted DESC

--- model/articles.model.js
+++ model/articles.model.js
@@ -4,7 +4,7 @@ exports.fetchAllArticles = () => {
   return db
     .query(
       `SELECT articles.*,CAST(COUNT(comments.article_id) AS INTEGER) AS comment_count FROM comments
-     JOIN articles ON articles.article_id=comments.article_id
+  FULL OUTER JOIN articles ON articles.article_id=comments.article_id
      GROUP BY articles.article_id ORDER BY articles.created_at DESC;`
     )
     .then((data) => {
