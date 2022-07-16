eda41292b5c8d476a17021ee41bd170b604af5fb
Adil Bulsari <adilbulsari@Adils-MacBook-Air.local>
2022-07-16 23:14:14 +0100

    added hereko

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,19 @@
-const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+const { Pool } = require("pg");
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
+const config =
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl:process.env.DATABASE_SSL === `true`
+      }
+    : {};
 
-module.exports = new Pool();
+module.exports = new Pool(config);
