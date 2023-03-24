2efa76e360f68b7ce4b298239eaade9f11961d4c
AdilBulsari <adilbulsari@gmail.com>
2023-03-24 09:44:20 +0000

    fixes production deployment issue

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,10 @@
 const ENV = process.env.NODE_ENV || "development";
 const { Pool } = require("pg");
 
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
